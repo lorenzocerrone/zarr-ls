78ad098f26c236ad73fdf1ee56cfb156d23d70f2
lorenzocerrone <lorenzo.cerrone@gmail.com>
2024-02-21 15:57:23 +0100

    first working version

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,41 @@
+use indexmap::IndexMap;
 use inquire::Select;
-use std::collections::HashMap;
-use std::fmt::Error;
-use std::fs::read_dir;
-use std::path::{Path, PathBuf};
-use std::sync::Arc;
+use zarrs::array::ArrayMetadata;
+use zarrs::array::ArrayMetadataV3;
+use zarrs::group::GroupMetadata;
+use zarrs::group::GroupMetadataV3;
 use zarrs::metadata;
-use zarrs::node::Node;
-use zarrs::node::NodePath;
-use zarrs::storage::store::{self, FilesystemStore};
-use zarrs::storage::{get_child_nodes, meta_key};
+use zarrs::node::NodeName;
+use zarrs::serde_json::to_string;
+
+use serde_json;
+use std::collections::LinkedList;
+use std::fs;
+use std::path::PathBuf;
+use std::sync::Arc;
+
+use zarrs::node::{Node, NodeMetadata};
+
+use zarrs::storage::store::FilesystemStore;
+
+static ZARR_GROUP_HEADER: &str = "Zarr Group: ";
+static ZARR_ARRAY_HEADER: &str = "Zarr Array: ";
+static ZARR_EXTENSION: &str = "zarr";
+static EXIT: &str = "Exit!";
+static BACK: &str = "..";
+
 
 #[derive(Debug, Clone)]
 enum CurrentSelection {
     Directory(PathBuf),
     ZarrFile(Node),
     Exit,
+    Back,
 }
 
-fn find_files(path: PathBuf) -> Vec<PathBuf> {
+fn read_dir(path: PathBuf) -> Vec<PathBuf> {
     assert!(path.exists());
-    assert!(path.is_dir());
-
-    let paths = read_dir(path);
+    let paths = fs::read_dir(path);
     paths
         .unwrap()
         .map(|entry| entry.unwrap().path())
@@ -38,48 +52,128 @@ fn find_files(path: PathBuf) -> Vec<PathBuf> {
         .collect::<Vec<PathBuf>>()
 }
 
-fn find_files_options(path: PathBuf) -> HashMap<String, CurrentSelection> {
-    let paths: Vec<PathBuf> = find_files(path.clone());
-    let mut matching_name: HashMap<String, CurrentSelection> = HashMap::new();
+fn find_files_options(path: PathBuf) -> IndexMap<String, CurrentSelection> {
+    let paths: Vec<PathBuf> = read_dir(path.clone());
+    let mut matching_name: IndexMap<String, CurrentSelection> = IndexMap::new();
 
     for path in paths {
         let path_name = path.file_name().unwrap().to_str().unwrap().to_string();
-        let current_selection = if path.ends_with(".zarr") {
-            CurrentSelection::Directory(path)
-        } else {
+        let path_extension = match path.extension() {
+            Some(ext) => ext.to_str().unwrap(),
+            None => "",
+        };
+        let current_selection = if path_extension == ZARR_EXTENSION {
             let store = Arc::new(FilesystemStore::new(path).unwrap());
             let node = Node::new(&*store, "/").unwrap();
             CurrentSelection::ZarrFile(node)
+        } else {
+            CurrentSelection::Directory(path)
         };
         matching_name.insert(path_name, current_selection);
     }
 
     // Special options
-    let parent_path = path.parent().unwrap().to_path_buf();
-    matching_name.insert(
-        "..".to_string(),
-        CurrentSelection::Directory(parent_path.clone()),
-    );
-    matching_name.insert("Exit!".to_string(), CurrentSelection::Exit);
-    return matching_name;
+    //let parent_path = path.parent().unwrap().to_path_buf();
+    //matching_name.insert(
+    //    "..".to_string(),
+    //    CurrentSelection::Directory(parent_path.clone()),
+    //);
+    matching_name.insert(BACK.to_string(), CurrentSelection::Back);
+    matching_name.insert(EXIT.to_string(), CurrentSelection::Exit);
+    matching_name
 }
 
-fn find_zarr_options(node: Node) -> HashMap<String, CurrentSelection> {
-    let mut matching_name: HashMap<String, CurrentSelection> = HashMap::new();
+fn format_array_infos(node: &Node, verbose: bool) -> String {
+    let name = node.path();
+    let metadata: &ArrayMetadata = match node.metadata() {
+        NodeMetadata::Array(array_metadata) => array_metadata,
+        NodeMetadata::Group(_) => panic!("This should not be a group"),
+    };
+    let infos: String = match metadata {
+        ArrayMetadata::V3(array_metadata) => {
+            let shape = &array_metadata.shape;
+            let dtype = array_metadata.data_type.name();
+
+            let more_info = if verbose {
+                format!(
+                    "\n{}",
+                    serde_json::to_string_pretty(&array_metadata).unwrap()
+                )
+            } else {
+                "".to_string()
+            };
+
+            format!("{:?} - {} {}", shape, dtype, more_info)
+        }
+    };
+
+    if verbose {
+        format!("{}{}{}", ZARR_ARRAY_HEADER, name, infos)
+    } else {
+        format!("{}{}", name, infos)
+    }
+}
+
+fn format_group_infos(node: &Node) -> String {
+    let name = node.path();
+    let child_headers = format!("\n  {:-<1$}>", "", ZARR_GROUP_HEADER.len() - 1);
+    let metadata: &GroupMetadata = match node.metadata() {
+        NodeMetadata::Group(group_metadata) => group_metadata,
+        NodeMetadata::Array(_) => panic!("This should not be an array"),
+    };
+
+    let infos: String = match metadata {
+        GroupMetadata::V3(group_metadata) => {
+            let num_children = node.children().len();
+            let mut child_infos: String = "".to_string();
+
+            for child in node.children() {
+                let child_metadata = child.metadata();
+                let _child_infos = match child_metadata {
+                    NodeMetadata::Array(_) => format_array_infos(child, false),
+                    NodeMetadata::Group(_) => format_group_infos(child),
+                };
+
+                child_infos.push_str(&child_headers);
+                child_infos.push_str(&_child_infos);
+            }
+            format!(
+                "{}{} - contains {} elements {}",
+                ZARR_GROUP_HEADER, name, num_children, child_infos
+            )
+        }
+    };
+    infos
+}
+
+fn format_zarr_option_name(node: Node) -> String {
+    let infos = match node.metadata() {
+        NodeMetadata::Array(_) => format_array_infos(&node, true),
+        NodeMetadata::Group(_) => format_group_infos(&node),
+    };
+    infos
+}
+
+fn find_zarr_options(node: Node) -> IndexMap<String, CurrentSelection> {
+    let mut matching_name: IndexMap<String, CurrentSelection> = IndexMap::new();
     for child in node.children() {
-        let child_name = child.name().to_string();
+        let child_name = format_zarr_option_name(child.clone());
         let current_selection = CurrentSelection::ZarrFile(child.clone());
         matching_name.insert(child_name, current_selection);
     }
-    matching_name.insert("Exit!".to_string(), CurrentSelection::Exit);
-    return matching_name;
+
+    matching_name.insert(BACK.to_string(), CurrentSelection::Back);
+    matching_name.insert(EXIT.to_string(), CurrentSelection::Exit);
+
+    matching_name
 }
 
 fn build_menu(current_path: CurrentSelection) -> CurrentSelection {
-    let matching_selections: HashMap<String, CurrentSelection> = match current_path {
+    let matching_selections: IndexMap<String, CurrentSelection> = match current_path {
         CurrentSelection::Directory(dir_path) => find_files_options(dir_path),
         CurrentSelection::ZarrFile(zarr_path) => find_zarr_options(zarr_path),
         CurrentSelection::Exit => panic!("Exit should not be a valid option"),
+        CurrentSelection::Back => panic!("Back should not be a valid option"),
     };
 
     let options: Vec<String> = matching_selections.keys().cloned().collect();
@@ -92,13 +186,29 @@ fn build_menu(current_path: CurrentSelection) -> CurrentSelection {
 }
 
 fn main() {
-    let mut path: PathBuf = PathBuf::from("/home/lcerrone/").canonicalize().unwrap();
-    let mut current_selection: CurrentSelection = CurrentSelection::Directory(path);
+    let path: PathBuf = PathBuf::from("/home/lcerrone/").canonicalize().unwrap();
+    let mut selections: LinkedList<CurrentSelection> = LinkedList::new();
+    selections.push_back(CurrentSelection::Directory(path.clone()));
+
     loop {
-        match current_selection {
+        let current_selection = selections.back();
+
+        let current_selection = match current_selection {
+            Some(selection) => selection,
+            None => panic!("No selection found"),
+        };
+
+        let next_selection = build_menu(current_selection.clone());
+
+        match next_selection {
             CurrentSelection::Exit => break,
+            CurrentSelection::Back => {
+                if selections.len() > 1 {
+                    selections.pop_back();
+                }
+            }
             _ => {
-                current_selection = build_menu(current_selection);
+                selections.push_back(next_selection);
             }
         }
     }
