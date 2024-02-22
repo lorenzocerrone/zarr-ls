5d711f3af4fbb669966c161c36ea8dda65e780a0
lorenzocerrone <lorenzo.cerrone@gmail.com>
2024-02-22 23:22:39 +0100

    fmt

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ use zarr_ls::filesystem_utils::DirNode;
 use zarr_ls::walkers::Walker;
 
 fn main() {
-    
     let initial_selection = ActionSelect::Dir(DirNode::new(&std::env::current_dir().unwrap()));
     let mut walker = Walker::new(&initial_selection);
     walker.run();
